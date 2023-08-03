bd10f7588da9f2d1b1db5debd0bf4d3a7399fea8
Tran Duc Khang <79127891+khangtranduc@users.noreply.github.com>
2023-08-03 19:05:30 +0800

    full screen canvas

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,8 @@
     let m =  { x: 0, y: 0 }
     let ctx: CanvasRenderingContext2D;
     let draw = false;
+    $: innerHeight = 0;
+    $: innerWidth = 0;
     onMount(() => {
         let tmp = canvas.getContext("2d");
         if (tmp != null) ctx = tmp;
@@ -11,20 +13,13 @@
     });
 </script>
 
+<svelte:window bind:innerHeight bind:innerWidth/>
 
-<canvas bind:this={canvas} on:mousemove={(e) => {
+<canvas bind:this={canvas} width={innerWidth} height={innerHeight} on:mousemove={(e) => {
     m.x = e.clientX;
     m.y = e.clientY;
     if (draw) ctx.fillRect(m.x, m.y, 5, 5)
 }} on:mousedown={() => draw = true} on:mouseup={() => draw = false}/>
-<a href={"#"} role="button" on:click={() => {
+<!-- <a href={"#"} role="button" on:click={() => {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-}}>Reset</a>
-
-
-<style lang="scss">
-    canvas {
-        border: solid;
-        margin: 0;
-    }
-</style>
+}}>Reset</a> -->
