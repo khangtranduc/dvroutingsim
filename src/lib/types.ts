ef8a97291a89e39c0543ea5b0daa46c592ca2aa1
Tran Duc Khang <79127891+khangtranduc@users.noreply.github.com>
2023-08-07 21:42:52 +0800

    implemented edges

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,9 +1,24 @@
+export type DistanceElement = {
+    dest: Node,
+    next?: Node,
+    cost: number,
+}
+
 export type Node = {
+    id: number,
     x: number,
     y: number,
     radius: number,
     fillStyle: string,
     strokeStyle: string,
-    selectedFill: string
+    selectedFill: string,
+    highStroke: string,
+    distanceVector?: DistanceElement[]
     selected?: boolean,
+    highlighted?: boolean
+}
+
+export type Edge = {
+    nodes: Node[],
+    cost: number
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import type { Node } from "$lib/types";
+    import { neighbours, other } from "$lib/dv";
+    import type { Node, Edge } from "$lib/types";
     let canvas: HTMLCanvasElement;
     let ctx: CanvasRenderingContext2D;
     let nodes: Node[] = [];
+    let edges: Edge[] = [];
     let selection: any;
+    let createEdge = false;
+    let edgeCost: any;
+    let fromNode: Node|null;
+    let toNode: Node|null;
     const within = (x: number, y: number) => {
         return nodes.find(n => {
             return x > (n.x - n.radius) &&
@@ -19,23 +25,44 @@
     }
     const drawNode = (node: Node) => {
         ctx.beginPath();
-        ctx.fillStyle = node.fillStyle;
+        ctx.fillStyle = node.selected ? node.selectedFill : node.fillStyle;
         ctx.arc(node.x, node.y, node.radius, 0, Math.PI * 2, true);
-        ctx.strokeStyle = node.strokeStyle;
+        ctx.strokeStyle = node.highlighted ? node.highStroke : node.strokeStyle;
+        ctx.lineWidth = node.highlighted ? 3 : 1;
         ctx.stroke();
         ctx.fill();
+        ctx.fillStyle = '#ffffff';
+        ctx.textAlign="center";
+        ctx.fillText(node.id.toString(), node.x, node.y);
     }
     const draw = () => {
         ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-        for (let i = 0; i < nodes.length; i++) {
-            let node = nodes[i];
+        //Draw Nodes
+        for (let i = 0; i < nodes.length; i++)
+            drawNode(nodes[i]);
+        //Draw Edges
+        for (let i = 0; i < edges.length; i++) {
+            let fromNode = edges[i].nodes[0];
+            let toNode = edges[i].nodes[1];
+            ctx.lineWidth = 1;
             ctx.beginPath();
-            ctx.fillStyle = node.selected ? node.selectedFill : node.fillStyle;;
-            ctx.arc(node.x, node.y, node.radius, 0, Math.PI * 2, true);
-            ctx.strokeStyle = node.strokeStyle;
-            ctx.fill();
+            ctx.strokeStyle = fromNode.strokeStyle;
+            ctx.moveTo(fromNode.x, fromNode.y);
+            ctx.lineTo(toNode.x, toNode.y);
             ctx.stroke();
+            ctx.fillStyle = '#000000'
+            ctx.fillText(edges[i].cost.toString(), (toNode.x + fromNode.x)/2, (toNode.y + fromNode.y)/2)
+        }
     }
+    const addEdge = (from: Node, to: Node, cost: number) => {
+        let nodes: Node[] = [from, to];
+        let edge: Edge = { nodes, cost };
+        if (!edges.find((e) => (e.nodes.includes(from) && e.nodes.includes(to)))) 
+            edges = [...edges, edge];
+        fromNode!.highlighted = undefined;
+        fromNode = null;
+        toNode = null;
+        draw();
     }
     onMount(() => {
         let tmp = canvas.getContext("2d");
@@ -43,45 +70,100 @@
         ctx.fillStyle = "rgb(200, 0, 0)";
         resize();
     });
+    $: console.log(neighbours(nodes[0], edges).map((x) => `${x.dest.id}, ${x.cost}`))
 </script>
 
-<svelte:window 
-    on:resize={resize} 
+{#if createEdge}
+<dialog open>
+    <article>
+        <header>
+            <a class="close" href={'#'} on:click={() => {createEdge = false}}> </a>
+            Create Edge
+        </header>
+        <label>
+            Edge cost
+            <input type="number" placeholder="Enter edge cost" bind:value={edgeCost} required/>
+        </label>
+        <button on:click={() => {
+            if (edgeCost 
+                && fromNode
+                && toNode) addEdge(fromNode, toNode, edgeCost);
+            createEdge = false;
+            edgeCost = null;
+        }}>
+            Add Edge
+        </button>
+    </article>
+</dialog>
+{/if}
+
+<svelte:window on:resize={resize}/>
+
+<canvas bind:this={canvas}
     on:mousedown={(e) => {
         let target = within(e.x, e.y);
+        if (selection && selection.selected){
+            selection.selected = false;
+        }
         if (target) {
-            selection = target
+            if (e.button == 1 && !fromNode) {
+                fromNode = target;
+                fromNode.highlighted = true;
+            }
+            else if (e.button == 1 && fromNode) {
+                if (fromNode != target){
+                    toNode = target;
+                    if (!edges.find((e) => fromNode && 
+                        toNode && 
+                        e.nodes.includes(fromNode) && 
+                        e.nodes.includes(toNode))){
+                        createEdge = true;
+                    }
+                }
+                else {
+                    fromNode.highlighted = false;
+                    fromNode = null;
+                }
+            }
+            selection = target;
             selection.selected = true;
+            draw();
         };
     }}
     on:mousemove={(e) => {
-        if (selection){
+        if (selection && e.buttons){
             selection.x = e.x;
             selection.y = e.y;
-            selection.moving = true;
             draw();
         }
     }}
     on:mouseup={(e) => {
-        if (!selection || !selection.moving) {
+        if (!selection) {
             let node = {
+                id: nodes.length,
                 x: e.x,
                 y: e.y,
                 radius: 10,
                 fillStyle: '#22cccc',
                 strokeStyle: '#009999',
-                selectedFill: '#88aaaa'
+                selectedFill: '#88aaaa',
+                highStroke: '#ff0000'
             };
-            nodes.push(node);
-            draw();
+            nodes = [...nodes, node];
+            drawNode(node);
         }
-        if (selection) {
-            delete selection.moving;
-            delete selection.selected;
+        if (selection && !selection.selected) {
+            selection = undefined;
         }
-        selection = undefined;
         draw();
     }}
 />
 
-<canvas bind:this={canvas}/>
+<style lang="scss">
+    header {
+        margin-bottom: 1rem;
+    }
+    article {
+        padding-bottom: 1rem;
+    }
+</style>
