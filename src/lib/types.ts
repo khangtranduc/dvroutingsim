5188b28b5356f85838ab7f1a39883c7ed5d43d0f
Tran Duc Khang <79127891+khangtranduc@users.noreply.github.com>
2023-08-07 14:04:50 +0800

    implemented node adding

---
+++ src/lib/types.ts
@@ -0,0 +1,9 @@
+export type Node = {
+    x: number,
+    y: number,
+    radius: number,
+    fillStyle: string,
+    strokeStyle: string,
+    selectedFill: string
+    selected?: boolean,
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,87 @@
 <script lang="ts">
     import { onMount } from "svelte";
+    import type { Node } from "$lib/types";
     let canvas: HTMLCanvasElement;
-    let m =  { x: 0, y: 0 }
     let ctx: CanvasRenderingContext2D;
-    let draw = false;
-    $: innerHeight = 0;
-    $: innerWidth = 0;
+    let nodes: Node[] = [];
+    let selection: any;
+    const within = (x: number, y: number) => {
+        return nodes.find(n => {
+            return x > (n.x - n.radius) &&
+                y > (n.y - n.radius) &&
+                x < (n.x + n.radius) &&
+                y < (n.y + n.radius);
+        })
+    }
+    const resize = () => {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    }
+    const drawNode = (node: Node) => {
+        ctx.beginPath();
+        ctx.fillStyle = node.fillStyle;
+        ctx.arc(node.x, node.y, node.radius, 0, Math.PI * 2, true);
+        ctx.strokeStyle = node.strokeStyle;
+        ctx.stroke();
+        ctx.fill();
+    }
+    const draw = () => {
+        ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+        for (let i = 0; i < nodes.length; i++) {
+            let node = nodes[i];
+            ctx.beginPath();
+            ctx.fillStyle = node.selected ? node.selectedFill : node.fillStyle;;
+            ctx.arc(node.x, node.y, node.radius, 0, Math.PI * 2, true);
+            ctx.strokeStyle = node.strokeStyle;
+            ctx.fill();
+            ctx.stroke();
+    }
+    }
     onMount(() => {
         let tmp = canvas.getContext("2d");
         if (tmp != null) ctx = tmp;
         ctx.fillStyle = "rgb(200, 0, 0)";
+        resize();
     });
 </script>
 
-<svelte:window bind:innerHeight bind:innerWidth/>
+<svelte:window 
+    on:resize={resize} 
+    on:mousedown={(e) => {
+        let target = within(e.x, e.y);
+        if (target) {
+            selection = target
+            selection.selected = true;
+        };
+    }}
+    on:mousemove={(e) => {
+        if (selection){
+            selection.x = e.x;
+            selection.y = e.y;
+            selection.moving = true;
+            draw();
+        }
+    }}
+    on:mouseup={(e) => {
+        if (!selection || !selection.moving) {
+            let node = {
+                x: e.x,
+                y: e.y,
+                radius: 10,
+                fillStyle: '#22cccc',
+                strokeStyle: '#009999',
+                selectedFill: '#88aaaa'
+            };
+            nodes.push(node);
+            draw();
+        }
+        if (selection) {
+            delete selection.moving;
+            delete selection.selected;
+        }
+        selection = undefined;
+        draw();
+    }}
+/>
 
-<canvas bind:this={canvas} width={innerWidth} height={innerHeight} on:mousemove={(e) => {
-    m.x = e.clientX;
-    m.y = e.clientY;
-    if (draw) ctx.fillRect(m.x, m.y, 5, 5)
-}} on:mousedown={() => draw = true} on:mouseup={() => draw = false}/>
-<!-- <a href={"#"} role="button" on:click={() => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-}}>Reset</a> -->
+<canvas bind:this={canvas}/>
